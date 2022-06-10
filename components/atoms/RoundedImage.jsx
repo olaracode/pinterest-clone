e17ec958ee09406b71b5e682db8534a71aee37f4
olaracode <octavioasapchi@gmail.com>
2022-06-10 00:35:18 -0400

    [😖 Add some things¿?]

---
+++ components/atoms/RoundedImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+const RoundedImage = ({ variant, src, ...props }) => {
+  const cardImg = variant === "hero" && { base: "120px", md: "320px" };
+  return (
+    <Box w={"100%"} h={"100%"} {...props}>
+      <Image src={src} w={"100%"} h={"100%"} borderRadius="20px" />
+    </Box>
+  );
+};
+
+export default RoundedImage;
---
+++ public/assets/utils/heroImgs.js
@@ -0,0 +1,16 @@
+const heroImgs = [
+    "/assets/imgs/landing/food/img1.jpg",
+    "/assets/imgs/landing/mode/img1.jpg",
+    "/assets/imgs/landing/nature/img1.jpg",
+    "/assets/imgs/landing/food/img2.jpg",
+    "/assets/imgs/landing/mode/img2.jpg",
+    "/assets/imgs/landing/nature/img2.jpg",
+    "/assets/imgs/landing/food/img3.png",
+    "/assets/imgs/landing/mode/img3.jpg",
+    "/assets/imgs/landing/nature/img5.jpg",
+    "/assets/imgs/landing/food/img4.png",
+    "/assets/imgs/landing/diy/img4.jpg",
+    "/assets/imgs/landing/nature/img4.jpg",
+]
+
+export default heroImgs
